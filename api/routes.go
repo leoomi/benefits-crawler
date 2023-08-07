f8594456c0f47ef93fce521d75771549c66d0c08
leoomi <leoyuuki@gmail.com>
2023-08-07 01:24:05 -0300

    Fix static files serving

--- api/routes.go
+++ api/routes.go
@@ -1,13 +1,24 @@
 package api
 
 import (
+	"os"
+
 	"github.com/gin-gonic/gin"
 )
 
 func (s *server) setupRoutes(rootDir string) {
 	router := gin.Default()
 
-	router.Static("/static", rootDir+"/client/build")
+	router.StaticFile("/", rootDir+"/client/build/index.html")
+	files, _ := os.ReadDir(rootDir + "/client/build")
+	for _, file := range files {
+		if file.Type().IsDir() || file.Name() == "index.html" {
+			continue
+		}
+
+		router.StaticFile("/"+file.Name(), rootDir+"/client/build/"+file.Name())
+	}
+	router.Static("/static", rootDir+"/client/build/static")
 	router.POST("/api/crawlerProcesses", s.createCrawler)
 	router.GET("/api/crawlerProcesses/:id", s.getCrawlerProcess)
 	router.GET("/api/benefits/:cpf", s.getBenefits)
